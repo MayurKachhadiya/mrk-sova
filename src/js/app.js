b7e0b9a2a1a2606f896c260e63150d5f7df7cfa9
Mayur Kachhadiya <mayurkachhadiya373@gmail.com>
2022-07-12 17:33:34 +0530

    mrkSOVASO

---
+++ src/js/app.js
@@ -0,0 +1,109 @@
+
+if(document.getElementsByClassName('tiny-three-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-three-item',
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        responsive: {
+            992: {
+                items: 3
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+
+window.addEventListener('load',   fn , false )
+
+//  window.onload = function loader() {
+function fn() {
+    // Preloader
+    if(document.getElementById('preloader')){
+        setTimeout(() => {
+            document.getElementById('preloader').style.visibility = 'hidden';
+            document.getElementById('preloader').style.opacity = '0';
+        }, 350);
+    }
+}
+//Menu
+function windowScroll() {
+  const navbar = document.getElementById("navbar");
+  if (
+    document.body.scrollTop >= 50 ||
+    document.documentElement.scrollTop >= 50
+  ) {
+    navbar.classList.add("nav-sticky");
+  } else {
+    navbar.classList.remove("nav-sticky");
+  }
+}
+window.addEventListener("scroll", (ev) => {
+  ev.preventDefault();
+  windowScroll();
+});
+
+// Navbar Active Class
+var spy = new Gumshoe('#navbar-navlist a', {
+ // Active classes
+ // navClass: 'active', // applied to the nav list item
+ // contentClass: 'active', // applied to the content
+ offset: 80
+});
+
+  // Toggle menu
+function toggleMenu() {
+  document.getElementById('isToggle').classList.toggle('open');
+  var isOpen = document.getElementById('navbar')
+  if (isOpen.style.display === "block") {
+      isOpen.style.display = "none";
+  } else {
+      isOpen.style.display = "block";
+  }
+};
+
+// back-to-top
+var mybutton = document.getElementById("back-to-top");
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if(mybutton!=null){
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//Feather icon
+feather.replace();
+
+//Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+  
